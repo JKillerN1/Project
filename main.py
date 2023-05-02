6f87bb8a2f3834d0c6bf442d4f2cba0128cf8ee0
Arina <arina2004popova@yandex.ru>
2023-05-02 10:47:14 +0300

    added graphs

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import os
 from tkinter import *
 from tkinter import ttk
+from PIL import ImageTk, Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 window = Tk()
 window.title("дневник")
@@ -34,14 +37,16 @@ def win(sportwindow, name_file):
     set = ttk.Treeview(sportwindow)
     set.pack()
 
-    set['columns'] = ('situation', 'emotion', 'think','mark')
+    set['columns'] = ('data', 'situation', 'emotion', 'think', 'mark')
     set.column("#0", width=0, stretch=NO)
+    set.column("data", anchor=CENTER, width=200)
     set.column("situation", anchor=CENTER, width=200)
     set.column("emotion", anchor=CENTER, width=200)
     set.column("think", anchor=CENTER, width=200)
     set.column("mark", anchor=CENTER, width=200)
 
     set.heading("#0", text="", anchor=CENTER)
+    set.heading("data", text="Дата", anchor=CENTER)
     set.heading("situation", text="Ситуация", anchor=CENTER)
     set.heading("emotion", text="Эмоции", anchor=CENTER)
     set.heading("think", text="Мысли", anchor=CENTER)
@@ -55,47 +60,56 @@ def win(sportwindow, name_file):
     input_frame = Frame(sportwindow)
     input_frame.pack()
 
+    data = Label(input_frame, text="Дата")
+    data.grid(row=0, column=0)
+
     situation = Label(input_frame, text="Ситуация")
-    situation.grid(row=0, column=0)
+    situation.grid(row=0, column=1)
 
     emotion = Label(input_frame, text="Эмоции")
-    emotion.grid(row=0, column=1, padx=10)
+    emotion.grid(row=0, column=2)
 
     think = Label(input_frame, text="Мысли")
-    think.grid(row=0, column=2)
+    think.grid(row=0, column=3)
 
     mark = Label(input_frame, text="Оценка")
-    mark.grid(row=0, column=3)
+    mark.grid(row=0, column=4)
+
+    data_entry = Entry(input_frame)
+    data_entry.grid(row=1, column=0)
 
     situation_entry = Entry(input_frame)
-    situation_entry.grid(row=1, column=0)
+    situation_entry.grid(row=1, column=1)
 
     emotion_entry = Entry(input_frame)
-    emotion_entry.grid(row=1, column=1)
+    emotion_entry.grid(row=1, column=2)
 
     think_entry = Entry(input_frame)
-    think_entry.grid(row=1, column=2)
+    think_entry.grid(row=1, column=3)
 
     mark_entry = Entry(input_frame)
-    mark_entry.grid(row=1, column=3)
+    mark_entry.grid(row=1, column=4)
 
-    with open(f"files/{name_file}.txt", "r") as f:
+    with open(f"files/{name_file}", "r") as f:
         for i in f.readlines():
             set.insert(parent='', index='end', text='',
                        values=i)
 
     def input_record():
         set.insert(parent='', index='end', text='',
-                   values=(situation_entry.get(), emotion_entry.get(), think_entry.get(), mark_entry.get()))
-        sportdata.append([situation_entry.get(), emotion_entry.get(), think_entry.get(),mark_entry.get()])
+                   values=(data_entry.get(), situation_entry.get(), emotion_entry.get(), think_entry.get(),
+                           mark_entry.get()))
+        sportdata.append([data_entry.get(), situation_entry.get(), emotion_entry.get(), think_entry.get(),
+                          mark_entry.get()])
 
-        with open(f"files/{name_file}.txt", "a") as f:
+        with open(f"files/{name_file}", "a") as f:
             for x in sportdata:
                 for y in x:
                     f.write(y)
                     f.write(" ")
                 f.write("\n")
 
+        data_entry.delete(0,END)
         situation_entry.delete(0, END)
         emotion_entry.delete(0, END)
         think_entry.delete(0, END)
@@ -144,17 +158,56 @@ def graf():
 
     grafframe.pack(anchor=CENTER)
 
+    spisok = []
+    for filename in os.listdir("files"):
+        new_lst = {}
+        with open(os.path.join("files", filename), 'r') as f:
+            text = f.read()
+            for item in text.split('\n'):
+                new_lst[item.split(" ")[0]] = int(item.split(" ")[-1]) \
+                    if item.split(" ")[-1] in ['1','2','3'] else item.split(" ")[-1]
+            spisok.append(new_lst)
+
+    def draw(data):
+        names = list(data.keys())
+        values = list(data.values())
+
+        with plt.rc_context({'xtick.color': 'white'}):
+            fig, axs = plt.subplots(1, 2, figsize=(10, 3), sharey=True)
+            axs[0].bar(names, values)
+            # axs[1].scatter(names, values)
+            axs[1].plot(names, values)
+        # fig.suptitle('Categorical Plotting')
+        plt.yticks(np.arange(1,4,1))
+        plt.show()
+
+
+    def sport_graf():
+        data = spisok[2]
+        draw(data)
+
+    def life_graf():
+        data = spisok[1]
+        draw(data)
+
+    def family_graf():
+        data = spisok[0]
+        draw(data)
+
     sport1 = Button(
         grafframe,
         text='Спорт',
+        command=sport_graf
     )
     life1 = Button(
         grafframe,
         text='Личная жизнь',
+        command=life_graf
     )
     family11 = Button(
         grafframe,
         text='Семья',
+        command=family_graf
     )
 
     def close():
@@ -231,13 +284,15 @@ window.mainloop()
 
 
 
-
-spisok=[]
+'''spisok=[]
 for filename in os.listdir("files"):
+    new_lst={}
     with open(os.path.join("files", filename), 'r') as f:
         text = f.read()
-        new_lst = [item for item in text.split(' ') if item in ['1', '2', '3']]
-        print(new_lst)
+        for item in text.split('\n'):
+            new_lst[item.split(" ")[0]]=item.split(" ")[-1]
+            print(int(item.split(" ")[-1]) if item.split(" ")[-1] in ['1','2','3'] else item.split(" ")[-1])
 
-
-
+        spisok.append(new_lst)
+print(spisok)
+'''
